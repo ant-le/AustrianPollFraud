ea603097ee2d3e49d643042bb340c507a4048a7d
ant-le <lechuga.anton@gmail.com>
2021-12-05 14:42:35 +0100

    Update Code

--- pipeline.py
+++ pipeline.py
@@ -16,3 +16,22 @@ getter.getProcessedData()
 plotter = Plotter(getter.df)
 plotter.scatterplot("ÖVP_SPÖ")
 # %%
+df = getter.df[["Date", "ÖVP_SPÖ", "Institute_bin"]]
+df[df["Institute_bin"] == 1].plot(x="Date", y="ÖVP_SPÖ",marker=6)
+df[df["Institute_bin"] == 0].plot(x="Date", y="ÖVP_SPÖ", kind='scatter')
+
+# %%
+import matplotlib.pyplot as plt
+import datetime as dt
+
+fig = plt.figure()
+ax = plt.axes()
+ax.scatter("Date", "ÖVP", marker=6, data=getter.df[getter.df["Institute_bin"]==1], label="Research Affairs")
+ax.scatter("Date", "ÖVP", marker=6, data=getter.df[getter.df["Institute_bin"]==0], label="Other Institutes")
+ax.set_xlabel('Date')
+ax.set_ylabel("Percentage Points")
+ax.axvline(dt.datetime(2017, 5, 10), c='grey', ls="--", label='Amtsübernahme')
+ax.legend()
+ax.set_title('Difference in Voting Percentage Estimation')
+
+# %%
