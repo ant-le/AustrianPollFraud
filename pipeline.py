e372cc65401010a63653582d2f5a855f075b00d6
ant-le <lechuga.anton@gmail.com>
2022-01-26 22:25:04 +0100

    Restructure of Data & Code (2)

--- pipeline.py
+++ pipeline.py
@@ -2,32 +2,36 @@
 # SetUp Logging
 import logging
 
+from matplotlib.pyplot import plot
+
 logging.basicConfig(
     format="%(levelname)s\t %(asctime)s\t %(message)s",
     datefmt="%m/%d/%Y %H:%M:%S",
     level=logging.INFO, # change to logging.ERROR to only log errors
 )
 # -------------------------- imports -------------------------- #
-from scripts.config import Configurator
-from scripts.data_scraper import Scraper
-from scripts.data_preprocessor import Preprocessor
-from model.differences import Diff_in_Diff_Model
+from scripts.scraper import Scraper
+from scripts.preprocessor import Preprocessor
+from scripts.plotter import Plotter
+from model.diff_in_diff import Diff_in_Diff_Regression
 # ------------------------- init objs ------------------------- #
-configurator = Configurator(type="raw", url="all")
-scraper = Scraper(config=configurator)
-preprocessor = Preprocessor(config=configurator)
-model1 = Diff_in_Diff_Model(var="ÖVP")
+scraper = Scraper()
+preprocessor = Preprocessor()
+plotter = Plotter()
+model1 = Diff_in_Diff_Regression()
 # ------------------------ run pipeline ------------------------ #
-def run_pipeline():
-    polls = scraper.load()
-    preprocessor.update(polls)
-    au_polls = preprocessor.load()
-    model1.fitData(au_polls["neuwal"])
+def run_pipeline(type="processed"): # Change to 'raw' or 'scrape'
+    if type is "processed":
+        polls = preprocessor.loadProcessed()
+    if type is "scrape":
+        polls = preprocessor.load(scraper)
+    elif type is "raw":
+        polls = preprocessor.load()    
+       
+    model1.fitData(polls)
     model1.ols_regression()
     model1.summary()
 
 
 if __name__== "__main__":
     run_pipeline()
-    
-preprocessor.df[configurator.url][preprocessor.df[configurator.url].Date.dt.month == 5]
